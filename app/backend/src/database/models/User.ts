7a1c1c8dd5f2b1481faa7782ee7a6d2a4e9e1b90
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-09 18:11:21 -0300

    feat(models): :sparkles: criando o model para o User

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,40 @@
+import { INTEGER, Model, STRING } from 'sequelize'
+import db from '.'
+
+class User extends Model {
+  id!: number
+  title!: string
+  price!: number
+  author!: string
+  isbn!: string
+}
+
+User.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: STRING,
+      allowNull: false,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+    },
+    password: {
+      type: STRING(100),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+  }
+)
+
+export default User
