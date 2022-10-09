eb8990789f6745360e146ac35b24191b9b1c08a9
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-09 19:52:54 -0300

    refactor(sequelize): :recycle: inserindo senha do bd

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize'
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
+  password: process.env.DB_PASS || '123456',
   database: process.env.DB_NAME || 'allugator',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
