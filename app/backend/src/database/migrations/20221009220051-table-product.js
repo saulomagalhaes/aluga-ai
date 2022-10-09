2c4ba3cd7aaf6005030b67246b87f67dff6c7473
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-09 19:52:24 -0300

    feat(migrations): :sparkles: criando migrations

---
+++ app/backend/src/database/migrations/20221009220051-table-product.js
@@ -0,0 +1,24 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('products')
+  },
+}
---
+++ app/backend/src/database/migrations/20221009220057-table-signature.js
@@ -0,0 +1,36 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('signatures', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'product_id',
+        references: {
+          model: 'products',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+    })
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('signatures')
+  },
+}
