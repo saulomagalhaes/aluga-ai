7bf7aca86510fefa0e7ecd0924fa93060dfd20f3
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-09 18:59:26 -0300

    feat(models): :sparkles: criando model Signature

---
+++ app/backend/src/database/models/Signature.ts
@@ -0,0 +1,35 @@
+import { DECIMAL, INTEGER, Model, STRING } from 'sequelize'
+import db from '.'
+
+class Signature extends Model {
+  id!: number
+  userId!: string
+  productId!: number
+}
+
+Signature.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: STRING,
+      allowNull: false,
+    },
+    productId: {
+      type: DECIMAL(10, 2),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'products',
+    timestamps: true,
+    underscored: true,
+  }
+)
+
+export default Signature
