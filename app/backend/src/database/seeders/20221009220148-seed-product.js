a881e218db2dcd1cf68b193958610c0bce31ffbd
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-10 20:14:16 -0300

    refactor(seeds): :recycle: alterando seeeds

--- app/backend/src/database/seeders/20221009220148-seed-product.js
+++ app/backend/src/database/seeders/20221009220148-seed-product.js
@@ -6,18 +6,22 @@ module.exports = {
         {
           name: 'Iphone 12',
           price: 2500,
+          img: 'http://localhost:3001/images/arquivo_1665431374708.png',
         },
         {
           name: 'Iphone 13',
           price: 3500,
+          img: 'http://localhost:3001/images/arquivo_1665431380967.png',
         },
         {
           name: 'Iphone 13 Pro',
           price: 4500,
+          img: 'http://localhost:3001/images/arquivo_1665431389248.png',
         },
         {
           name: 'Iphone 13 Pro Max',
           price: 5500,
+          img: 'http://localhost:3001/images/arquivo_1665431393295.png',
         },
       ],
       {}
--- app/backend/src/database/seeders/20221009220153-seed-signature.js
+++ app/backend/src/database/seeders/20221009220153-seed-signature.js
@@ -6,14 +6,23 @@ module.exports = {
         {
           user_id: 1,
           product_id: 4,
+          img_url: 'https://i.imgur.com/1ZQZQ0x.jpg',
+          created_at: new Date(),
+          updated_at: new Date(),
         },
         {
           user_id: 2,
           product_id: 3,
+          img_url: 'https://i.imgur.com/1ZQZQ0x.jpg',
+          created_at: new Date(),
+          updated_at: new Date(),
         },
         {
           user_id: 3,
           product_id: 1,
+          img_url: 'https://i.imgur.com/1ZQZQ0x.jpg',
+          created_at: new Date(),
+          updated_at: new Date(),
         },
       ],
       {}
