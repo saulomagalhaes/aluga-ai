3879d996d608c47989d141ebebae0ac82cb33d5b
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-10 20:21:07 -0300

    feat(routes): :sparkles: implementando rota get products id

--- app/backend/src/routes/product.routes.ts
+++ app/backend/src/routes/product.routes.ts
@@ -4,6 +4,8 @@ import { productController } from '.'
 const router = Router()
 
 router.get('/', (req, res) => productController.getAllProducts(req, res))
-router.get('/:id', (req, res) => productController.getProductById(req, res))
+router.get('/products/:id', (req, res) =>
+  productController.getProductById(req, res)
+)
 
 export default router
