5c9f02230d660df0f57f0dcd2f89529d293fb5a1
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-10 20:19:46 -0300

    feat(services): :sparkles: implementando o SignatureService e os métodos signatures e checkout

---
+++ app/backend/src/services/signatureService.ts
@@ -0,0 +1,46 @@
+import Product from '../database/models/Product'
+import Signature from '../database/models/Signature'
+import { ThrowError } from '../error/throwError'
+import { ISignatureService } from '../interfaces/ISignatureService'
+
+class SignatureService implements ISignatureService {
+  public signatures = async (userId: number): Promise<Signature[] | null> => {
+    const signatures = await Signature.findAll({
+      where: { userId },
+      attributes: ['createdAt'],
+      include: [
+        {
+          model: Product,
+          as: 'product',
+          attributes: ['name', 'price'],
+        },
+      ],
+    })
+    if (!signatures) {
+      throw new ThrowError('NotFound', 'No signatures found')
+    }
+    return signatures
+  }
+
+  public checkout = async (
+    userId: number,
+    productId: number,
+    imgUrl: string
+  ): Promise<void> => {
+    const product = await Product.findByPk(productId)
+    if (!product) {
+      throw new ThrowError('NotFound', 'Product not found')
+    }
+    const localImage = `http://localhost:3001/upload/${imgUrl}`
+    const signature = await Signature.create({
+      userId,
+      productId,
+      imgUrl: localImage,
+    })
+    if (!signature) {
+      throw new ThrowError('ConflictError', 'Signature already exists')
+    }
+  }
+}
+
+export { SignatureService }
