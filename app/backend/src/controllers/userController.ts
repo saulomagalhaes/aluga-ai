e9f8a259a8e109f587a22b860ae229e92a78f560
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-10 09:47:07 -0300

    feat(controllers): :sparkles: implementando Usercontroller e os métodos login e register

---
+++ app/backend/src/controllers/userController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+import { IUserService } from '../interfaces/IUserService'
+import { JoiService } from '../services/joiService'
+
+class UserController {
+  constructor(private _userService: IUserService) {}
+
+  public login = async (req: Request, res: Response): Promise<void> => {
+    await JoiService.validateBodyLogin(req.body)
+    const token = await this._userService.login(req.body)
+    res.status(200).json({ token })
+  }
+
+  public register = async (req: Request, res: Response): Promise<void> => {
+    await JoiService.validateBodyRegister(req.body)
+    const token = await this._userService.register(req.body)
+    res.status(201).json({ token })
+  }
+}
+
+export { UserController }
