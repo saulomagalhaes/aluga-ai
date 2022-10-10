dcf5dadd05afbc4353abf7ba39aff3204483c7a1
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-10 20:16:20 -0300

    feat(appExpress): :sparkles: implementando rotas estáticas

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -3,14 +3,19 @@ import express from 'express'
 import 'express-async-errors'
 import errorHandler from './middlewares/errorHandler'
 import productRouter from './routes/product.routes'
+import signatureRouter from './routes/signature.routes'
 import userRouter from './routes/user.routes'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/upload', express.static('uploads'))
+app.use('/images', express.static('public/images'))
 app.use('', userRouter)
 app.use('', productRouter)
+app.use('/signature', signatureRouter)
 
 app.use(errorHandler)
 
