da4c453223189b9ece189b30e0e77b581b6a9f7b
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-10 09:45:17 -0300

    feat(App.tsx): :sparkles: configurando o app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,9 +1,15 @@
-import cors from 'cors';
-import express from 'express';
+import cors from 'cors'
+import express from 'express'
+import 'express-async-errors'
+import errorHandler from './middlewares/errorHandler'
+import userRouter from './routes/user.routes'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+app.use('', userRouter)
 
-export default app;
+app.use(errorHandler)
+
+export default app
