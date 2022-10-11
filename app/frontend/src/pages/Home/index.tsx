7678e7e245fb239535d51170c81e391bab20d8e1
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-11 16:43:49 -0300

    feat(page): :sparkles: criando página home

---
+++ app/frontend/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../../components/Header'
+import { Products } from './components/Products'
+export function Home() {
+  return (
+    <>
+      <Header />
+      <Products />
+    </>
+  )
+}
