d02855b37e2abd050f285d7f1c0e95921202809d
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-11 16:46:08 -0300

    feat(page): :sparkles: página detalhes do produto

---
+++ app/frontend/src/pages/ProductDetail/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header'
+import { Product } from './components/Product'
+
+export function ProductDetail() {
+  return (
+    <>
+      <Header />
+      <Product />
+    </>
+  )
+}
