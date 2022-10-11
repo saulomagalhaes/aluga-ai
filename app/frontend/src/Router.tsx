10c8c941edcc4efa62fd164587dd67fa69a761be
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-11 16:34:21 -0300

    feat(Router.tsx): :sparkles: inserindo rotas do frontend

--- app/frontend/src/Router.tsx
+++ app/frontend/src/Router.tsx
@@ -1,11 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
-
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { ProductDetail } from './pages/ProductDetail'
+import { Register } from './pages/Register'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
-  );
+  )
 }
