9972a051611ace80ceadc2bf833c2e48496111a6
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-12 10:48:10 -0300

    feat(page): :sparkles: Cadastro de um usuário

---
+++ app/frontend/src/pages/Register/index.tsx
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { ContainerRegister } from './styles'
+
+const URL = import.meta.env.VITE_HOSTNAME
+const PORT = import.meta.env.VITE_BACKEND_PORT
+
+export function Register() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [error, setError] = useState('')
+
+  const saveUser = (value: string) =>
+    localStorage.setItem('token', JSON.stringify(value))
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    axios
+      .post(`http://${URL}:${PORT}/register`, {
+        name,
+        email,
+        password,
+      })
+      .then((response) => {
+        saveUser(response.data.token)
+      })
+      .catch((error) => {
+        setError(error.response.data.message)
+      })
+  }
+
+  return (
+    <ContainerRegister>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Nome</label>
+        <input
+          type="text"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password"> Senha </label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+        <p>{error}</p>
+      </form>
+    </ContainerRegister>
+  )
+}
---
+++ app/frontend/src/pages/Register/styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+export const ContainerRegister = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    background: ${(props) => props.theme['gray-300']};
+    padding: 8rem 6rem;
+    border-radius: 1rem;
+    box-shadow: 0 0 1rem ${(props) => props.theme['gray-300']};
+
+    label {
+      margin-top: 1rem;
+      font-size: 1.2rem;
+    }
+
+    input {
+      margin-top: 0.5rem;
+      border: 0;
+      border-radius: 0.25rem;
+      height: 2rem;
+      width: 15rem;
+      padding: 0 0.5rem;
+    }
+
+    button {
+      margin-top: 1rem;
+      font-size: 1rem;
+      border: 0;
+      color: ${(props) => props.theme.white};
+      background: ${(props) => props.theme['green-300']};
+      padding: 0.5rem 1rem;
+    }
+
+    p {
+      margin-top: 1rem;
+      font-size: 1rem;
+      color: ${(props) => props.theme['red-700']};
+      text-align: center;
+    }
+  }
+`
