f9ee6b3e9c6aad384adcd4840ce6e55e28191c4e
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-12 20:15:39 -0300

    feat(Router.tsx): :sparkles: adicionando rota para user, signature e checkout

--- app/frontend/src/Router.tsx
+++ app/frontend/src/Router.tsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
+import { Checkout } from './pages/Checkout'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import { ProductDetail } from './pages/ProductDetail'
 import { Register } from './pages/Register'
+import { Signatures } from './pages/Signatures'
+import { User } from './pages/User'
 
 export function Router() {
   return (
@@ -11,6 +14,9 @@ export function Router() {
       <Route path="/product/:id" element={<ProductDetail />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/user" element={<User />} />
+      <Route path="/signatures" element={<Signatures />} />
+      <Route path="/checkout" element={<Checkout />} />
     </Routes>
   )
 }
