071af83de60a1a4af6377671ea744046d456bcb4
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-12 20:27:00 -0300

    feat(signature): :sparkles: página para assinaturas criada

---
+++ app/frontend/src/pages/Signatures/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderLogged } from '../../components/HeaderLogged'
+import { MySignatures } from './components/MySignatures'
+
+export function Signatures() {
+  return (
+    <>
+      <HeaderLogged />
+      <MySignatures />
+    </>
+  )
+}
