dd5d76ad19a75634c5e5820753a629f430d6b33f
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-12 20:18:57 -0300

    feat(page): :sparkles: pagina para checkout

---
+++ app/frontend/src/pages/Checkout/index.tsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { HeaderLogged } from '../../components/HeaderLogged'
+import { Cart } from './components/cart'
+
+const URL = import.meta.env.VITE_HOSTNAME
+const PORT = import.meta.env.VITE_BACKEND_PORT
+
+export function Checkout() {
+  const navigate = useNavigate()
+  const [userId, setUserId] = useState('')
+  useEffect(() => {
+    const url = `http://${URL}:${PORT}/authorization`
+
+    if (!localStorage.getItem('token')) {
+      return navigate('/login')
+    }
+    const config = {
+      headers: {
+        authorization: JSON.parse(localStorage.getItem('token') || ''),
+      },
+    }
+    axios
+      .get<{ id: string }>(url, config)
+      .then((response) => {
+        setUserId(response.data.id)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+  return (
+    <>
+      <HeaderLogged />
+      <Cart userId={userId} />
+    </>
+  )
+}
