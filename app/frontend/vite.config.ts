d1b4171a539cd711a24302144ddd40b8b5cd0b61
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-13 13:14:55 -0300

    chore: :wrench: configuração do vite

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+      include: ['**/*.json', '**/*.less', '**/*.tsx'],
+    }),
+  ],
 })
