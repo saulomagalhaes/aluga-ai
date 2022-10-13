37a1fce098f29ae2b58e16531542795814928d11
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-13 16:40:56 -0300

    feat(routes): :sparkles: rota para filtros

--- app/backend/src/routes/product.routes.ts
+++ app/backend/src/routes/product.routes.ts
@@ -4,6 +4,9 @@ import { productController } from '.'
 const router = Router()
 
 router.get('/', (req, res) => productController.getAllProducts(req, res))
+router.get('/product', (req, res) =>
+  productController.getProductsFiltered(req, res)
+)
 router.get('/product/:id', (req, res) =>
   productController.getProductById(req, res)
 )
