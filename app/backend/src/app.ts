ed03c631f2996b2620ded340a4159da6ecfd3ad9
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-13 14:13:18 -0300

    feat(swagger): :sparkles: rota para documentacao

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,10 +1,12 @@
 import cors from 'cors'
 import express from 'express'
 import 'express-async-errors'
+import swaggerUi from 'swagger-ui-express'
 import errorHandler from './middlewares/errorHandler'
 import productRouter from './routes/product.routes'
 import signatureRouter from './routes/signature.routes'
 import userRouter from './routes/user.routes'
+import swaggerDocument from './swagger.json'
 
 const app = express()
 
@@ -16,6 +18,7 @@ app.use('/images', express.static('public/images'))
 app.use('', userRouter)
 app.use('', productRouter)
 app.use('/signature', signatureRouter)
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(errorHandler)
 
