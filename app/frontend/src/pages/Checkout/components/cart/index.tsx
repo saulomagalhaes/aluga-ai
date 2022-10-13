d0af5d6ed834ae0dc94edb6095e2f2b58cf89aae
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-13 13:21:29 -0300

    feat(cart): :sparkles: cria navegação

--- app/frontend/src/pages/Checkout/components/cart/index.tsx
+++ app/frontend/src/pages/Checkout/components/cart/index.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { IProductsInterface } from '../../../../interfaces/IProductsInterface'
+import { ContainerCart, Product } from './styles'
 
 const URL = import.meta.env.VITE_HOSTNAME
 const PORT = import.meta.env.VITE_BACKEND_PORT
@@ -8,6 +10,8 @@ const PORT = import.meta.env.VITE_BACKEND_PORT
 export function Cart(props: { userId: string }) {
   const storage = JSON.parse(localStorage.getItem('product') || '[]')
   const [file, setFile] = useState('')
+  const navigate = useNavigate()
+
   const handleFile = (e: any) => {
     setFile(e.target.files[0])
   }
@@ -26,26 +30,26 @@ export function Cart(props: { userId: string }) {
     formData.append('productId', storage[0].id)
     axios
       .post(url, formData, config)
-      .then((response) => {
-        console.error(response)
+      .then((_response) => {
+        navigate('/signatures')
       })
       .catch((_error) => {
         alert('Você não enviou o documento solicitado')
       })
   }
   return (
-    <div>
-      <div>
+    <ContainerCart>
+      <Product>
         {storage.map((product: IProductsInterface) => (
           <div key={product.id}>
             <h1>{product.name}</h1>
-            <h2>{product.price}</h2>
             <img src={product.img} alt="product" />
+            <h2>Valor Total: {product.price}</h2>
           </div>
         ))}
-      </div>
-      <button onClick={checkout}>Comprar</button>
-      <input type="file" name="arquivo" id="arquivo" onChange={handleFile} />
-    </div>
+        <button onClick={checkout}>Comprar</button>
+        <input type="file" name="arquivo" id="arquivo" onChange={handleFile} />
+      </Product>
+    </ContainerCart>
   )
 }
